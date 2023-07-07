41ebc5f513df55d0b18687ac2780d659a56bad45
Amol2802 <138649028+Amol2802@users.noreply.github.com>
2023-07-07 09:48:30 +0530

    Update AA.java
    added new line

--- demo3/src/main/java/testDemo/AA.java
+++ demo3/src/main/java/testDemo/AA.java
@@ -1,9 +1,12 @@
+
 package testDemo;
 
 public class AA {
 
 	public static void main(String[] args) {
 		System.out.println("I am in demo3 project");
+
+	    System.out.println("person B code"); 
 	}
 
 }
