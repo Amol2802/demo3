44dfa92863c1d585e5a5bf3d6578ef45c20a0424
JAYA VYAWHARE <JAYA VYAWHARE@DESKTOP-6G9M66L>
2023-07-08 21:43:34 +0530

    changes update

--- demo3/src/main/java/testDemo/AA.java
+++ demo3/src/main/java/testDemo/AA.java
@@ -5,7 +5,8 @@ public class AA {
 
 	public static void main(String[] args) {
 		System.out.println("I am in demo3 project");
-
+		System.out.println("I am in demo3 project 2nd commit");
+		
 	    System.out.println("person B code"); 
 	}
 
